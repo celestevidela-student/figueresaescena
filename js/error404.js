cd3de8c1a215b54d7ae8628583b6b5538b23964d
celestevidela-student <s24.cvidela@llotja.cat>
2026-06-12 00:36:32 +0200

    Update error404.js

--- js/error404.js
+++ js/error404.js
@@ -2,6 +2,6 @@ const homeButton = document.getElementById('tornar-inici');
 
 if (homeButton) {
   homeButton.addEventListener('click', () => {
-    window.location.href = 'inici.html';
+    window.location.href = 'index.html';
   });
 }
